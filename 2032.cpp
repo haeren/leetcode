75af39ee0e2b2b799d8235207a60f834f4bcd456
haeren <54928744+haeren@users.noreply.github.com>
2022-05-11 10:58:31 +0300

    Update 2032.cpp

--- 2032.cpp
+++ 2032.cpp
@@ -11,30 +11,36 @@ public:
         for (int i = 0; i < size1; i++)
         {
             for (int j = 0; j < size2; j++)
+            {
                 if (nums1[i] == nums2[j])
                 {
                     if (find(result.begin(), result.end(), nums2[j]) == result.end())
                         result.push_back(nums2[j]);
                     break;
                 }
+            }
             for (int j = 0; j < size3; j++)
+            {
                 if (nums1[i] == nums3[j])
                 {
                     if (find(result.begin(), result.end(), nums3[j]) == result.end())
                         result.push_back(nums3[j]);
                     break;
                 }
+            }
         }
         
         for (int i = 0; i < size2; i++)
         {
             for (int j = 0; j < size3; j++)
+            {
                 if (nums2[i] == nums3[j])
                 {
                     if (find(result.begin(), result.end(), nums3[j]) == result.end())
                         result.push_back(nums3[j]);
                     break;
                 }
+            }
         }
         
         return result;
